9b95abee868c3b106bc25c85c2ffdcea3de01964
Mohammed Shifreen <mshifreen@gmail.com>
2023-05-18 13:37:53 +0530

    PHPUnit configurations added and fixed AccountEmailTest class.

---
+++ test/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The file is part of the "webifycms/ext-user", WebifyCMS extension package.
+ *
+ * @see https://webifycms.com/extension/user
+ *
+ * @license Copyright (c) 2023 WebifyCMS
+ * @license https://webifycms.com/extension/user/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
+ */
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
