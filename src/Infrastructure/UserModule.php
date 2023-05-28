9d4195f03ea5aac72b900ffa79fad16eedd4bea5
Mohammed Shifreen <mshifreen@gmail.com>
2023-05-28 23:05:19 +0530

    Fixed administration and moved administration menu registeration to module class.

--- src/Infrastructure/UserModule.php
+++ src/Infrastructure/UserModule.php
@@ -1,31 +1,55 @@
 <?php
+/**
+ * The file is part of the "webifycms/ext-user", WebifyCMS extension package.
+ *
+ * @see https://webifycms.com/extension/user
+ *
+ * @license Copyright (c) 2022 WebifyCMS
+ * @license https://webifycms.com/extension/user/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
+ */
+declare(strict_types=1);
 
 namespace Webify\User\Infrastructure;
 
+use Webify\Admin\Infrastructure\AdminModule;
 use yii\base\Module;
 
 /**
- * Class UserModule
- *
- * @package webifycms/user
- * @version 0.0.1
- * @since   0.0.1
- * @author  Mohammed Shifreen
+ * User module.
  */
 class UserModule extends Module
 {
-    public $basePath = '@User';
+	public string $basePath = '@User';
 
-    public $layout = 'main';
+	public $layout = 'main';
 
-    public $controllerNamespace = 'Webify\\User\\Infrastructure\\Presentation\\Admin\\Controller';
+	public $controllerNamespace = 'Webify\\User\\Infrastructure\\Presentation\\Admin\\Controller';
 
-    /**
-     * @inheritDoc
-     */
-    public function init(): void
-    {
-        parent::init();
-        $this->setViewPath('@User/templates');
-    }
+	public function init(): void
+	{
+		parent::init();
+		$this->setViewPath('@User/templates');
+		$this->registerMenuItems();
+	}
+
+	/**
+	 * Register user module's admin menu items.
+	 */
+	private function registerMenuItems(): void
+	{
+		/**
+		 * @var AdminModule $module
+		 */
+		$module = $this->module;
+
+		$module->menuService->addItems([
+			[
+				'label'    => 'Users',
+				'icon'     => 'person-square',
+				'route'    => ["/{$module->id}/user"],
+				'position' => 1,
+			],
+		]);
+	}
 }
